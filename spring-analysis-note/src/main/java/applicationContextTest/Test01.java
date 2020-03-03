54ad055658f335827a46da157d404ebd9ce9a7df
wangxizzz <979675820@qq.com>
2020-03-03 13:43:53 +0800

    aware接口

--- spring-analysis-note/src/main/java/applicationContextTest/Test01.java
+++ spring-analysis-note/src/main/java/applicationContextTest/Test01.java
@@ -1,5 +1,6 @@
 package applicationContextTest;
 
+import aware接口实现.MyApplicationAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,7 +11,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Test01 {
 	public static void main(String[] args) {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+		MyApplicationAware myApplicationAware = (MyApplicationAware) applicationContext.getBean("myApplicationAware");
+		myApplicationAware.display();
 	}
 }
 
